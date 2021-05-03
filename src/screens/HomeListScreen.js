2aaea2d12eb247d65baf83dff62f2c9dc60f1fb6
Max de Vries <max.devries.1995@gmail.com>
2021-05-03 22:40:05 +0200

    Add a flatlist with all card components

--- src/screens/HomeListScreen.js
+++ src/screens/HomeListScreen.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { FloatingAction } from 'react-native-floating-action';
+import { useDispatch, useSelector } from 'react-redux'; 
 
 import Card from '../components/Card';
+import * as houseAction from '../redux/actions/houseAction';
 
 export default function HomeListScreen(props) {
+  const dispatch = useDispatch();
+  const { houses } = useSelector(state => state.house);
+
+  useEffect(() => {
+    dispatch(houseAction.fetchHouses());
+  }, [dispatch]);
+  
   return (
     <View style={styles.container}>
-      <Card />
+      <FlatList
+        data={houses}
+        keyExtractor={item => item._id}
+        renderItem={({item}) => (
+          <Card
+            navigation={props.navigation}
+            id={item._id}
+            title={item.title}
+            address={item.address}
+            homeType={item.homeType}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+            yearBuilt={item.yearBuilt}
+          />
+        )}
+      />
       <FloatingAction
         position="right"
         animated={false}
         showBackground={false}
-        onPressMain={() => console.log('clicked')}
+        onPressMain={() => props.navigation.navigate('AddHome')}
       />
     </View>
   );
