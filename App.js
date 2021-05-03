e60d41e50b8eee2f331a9838746e7e6ff1cef34d
Max de Vries <max.devries.1995@gmail.com>
2021-05-03 22:37:23 +0200

    Add a store

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 import AppNavigator from './src/navigation/AppNavigator';
+import store from './src/redux/store';
 
 export default function App() {
   return (
-    <AppNavigator />
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
 
